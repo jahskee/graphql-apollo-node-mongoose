1afab47ef7b119e90de60ad64ce85f2bb1cf5935
jahskee <jahskee@yahoo.com>
2018-06-23 05:01:33 -0400

    setup babel-watch

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 import express from 'express';
+const server = express();
+
+server.get("/graphql", (req, res) => {
+    res.send(`<html><head><body>
+                <h1>Hello World!</h1>
+                </body></head></html>`) 
+})
+
+server.listen(4000, () => {
+    console.log("listening on port 4000")
+})
