3dd333d7cbfd100fb695bb13b5b00cab44848036
jahskee <jahskee@yahoo.com>
2018-06-23 06:08:47 -0400

    add resolvers

--- schema.js
+++ schema.js
@@ -1,16 +1,17 @@
 import { makeExecutableSchema,  addMockFunctionsToSchema } from 'graphql-tools';
+import resolvers from './resolvers.js'
 
 const typeDefs = `type Author {
     age: Int
     name: String
-    Books: [String]
+    books: [String]
 }
     type Query {
         author: [Author]
     }
 `;
 
-const schema = makeExecutableSchema({typeDefs})
-addMockFunctionsToSchema({schema});
+const schema = makeExecutableSchema({typeDefs, resolvers})
+//addMockFunctionsToSchema({schema});
 
 export default schema;
