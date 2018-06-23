b322bdd0906e70f35a18958ff8122be18da6867a
jahskee <jahskee@yahoo.com>
2018-06-23 05:36:41 -0400

    add body-parser middleware to graphql

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import bodyParser from 'body-parser';
+
 const server = express();
 
 server.get("/graphql", (req, res) => {
@@ -7,6 +9,12 @@ server.get("/graphql", (req, res) => {
                 <h1>Hello World!</h1>
                 </body></head></html>`) 
 })
+
+server.use('/graphiql', graphiqlExpress({
+    endpointURL: "/graphql",
+}));
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({}))
 
 server.listen(4000, () => {
     console.log("listening on port 4000")
